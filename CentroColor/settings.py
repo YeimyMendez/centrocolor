85b0daecd9f0c6dc04cdc1fe0f196b0090e66168
yeimymendez <yfmendez5@gmail.com>
2022-10-18 15:16:34 -0500

    prueba 1 heroku

--- CentroColor/settings.py
+++ CentroColor/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'principal.Universal'
+# AUTH_USER_MODEL = 'principal.Usuario'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
